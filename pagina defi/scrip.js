a6aca4c263c1e02332b44cd9db7e929678ad07fa
DavidNe002 <juan.dmn2006@gmail.com>
2025-05-05 01:28:47 -0400

    Add files via upload

---
+++ pagina defi/scrip.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('mySwiper', {
+    slidesPerView: 1,
+    speceBetween: 80,
+    grabCursor: true,
+    loop: true,
+    breakpoints: {
+        991: {
+            slidesPerView: 3,
+        },
+});
